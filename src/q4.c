bb97d85099738580048602f7b25c7bdb81665be3
aadhikari-design <aadhikari@maharishi.org.np>
2025-09-04 18:49:19 +0545

    q4.c

--- src/q4.c
+++ src/q4.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-int main() {
-int i;
-printf("Even numbers between 1 and 100 are:\n");
-for(i = 2; i <= 100; i += 2) {
-printf("%d ", i);
+int main(){
+    int i;
+    for(i=2;i<=100;i+=2){
+        printf("%d ",i);
     }
- printf("\n");
-return 0;
+    return 0;
 }
