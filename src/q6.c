a5640485a7477874d07741f77d2cf9c784c4d87c
aadhikari-design <aadhikari@maharishi.org.np>
2025-09-04 18:55:52 +0545

    q6.c

--- src/q6.c
+++ src/q6.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
-
-int main() {
-    int num, i = 1;
-
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    printf("Multiplication table of %d:\n", num);
-
-    do {
-        printf("%d x %d = %d\n", num, i, num * i);
+int main(){
+    int num,i=1;
+    scanf("%d",&num);
+    do{
+        printf("%d x %d = %d\n",num,i,num*i);
         i++;
-    } while(i <= 10);
-
+    }while(i<=10);
     return 0;
 }
